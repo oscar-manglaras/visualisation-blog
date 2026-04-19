6a9c9064969c2a3a01f8e7787516e902abf99f00
oscar-manglaras <257900962+oscar-manglaras@users.noreply.github.com>
2026-04-19 18:11:40 +0930

    move data processing code to separate file

---
+++ pages/elections/house-of-reps/src/data-processing.ts
@@ -0,0 +1,84 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+
+type State = 'ACT'|'NSW'|'NT'|'QLD'|'SA'|'TAS'|'VIC'|'WA';
+
+interface ElectionResults {
+    year: number;
+    electorates: ElectorateResult[];
+}
+
+interface ElectorateResult {
+    state: State;
+    name: string;
+    results: RoundResult[][];
+    candidates: Candidate[]
+}
+
+interface Candidate {
+    given_name: string;
+    surname: string;
+    party_name: string;
+    party_abbr: string;
+    elected: boolean;
+    incumbent: boolean;
+    ballot_id: number;
+}
+
+interface RoundResult {
+    candidate: Candidate | undefined;
+    round: number;
+    count: number;
+    percentage: number;
+}
+
+export async function fetch_data() {
+    let text = await d3.text('./data/2025-federal-election.csv');
+    
+    // remove the extra header row
+    text = text.replace(/^[^\r\n]+\r?\n/, '');
+
+    const electorates = d3.flatGroup(d3.csvParse(text), d => d.StateAb, d => d.DivisionNm);
+
+    const election_results: ElectionResults = {
+        year: 2025,
+        electorates: electorates.map((d): ElectorateResult => {
+            const candidates = d3.flatGroup(d[2], d => d.BallotPosition)
+                .map(d => ({
+                    given_name: d[1]?.[0]?.GivenNm ?? '',
+                    surname: d[1]?.[0]?.Surname ?? '',
+                    party_name: d[1]?.[0]?.PartyNm ?? '',
+                    party_abbr: d[1]?.[0]?.PartyAb ?? '',
+                    ballot_id: parseInt(d[1]?.[0]?.BallotPosition ?? ''),
+                    elected: d[1]?.[0]?.Elected === 'Y',
+                    incumbent: d[1]?.[0]?.HistoricElected === 'Y',
+                }));
+
+            const results: RoundResult[][] = [];
+            const rawResults = d3.group(d[2], d => parseInt(d.CountNumber??''), d => parseInt(d.BallotPosition??''));
+            rawResults.forEach((d, round) => {
+                const round_results: RoundResult[] = [];
+
+                d.forEach((d, ballot_id) => {
+                    round_results.push({
+                        round: round,
+                        candidate: candidates.find(c => c.ballot_id === ballot_id),
+                        count: parseInt(d.find(d => d.CalculationType === 'Preference Count')?.CalculationValue ?? ''),
+                        percentage: parseInt(d.find(d => d.CalculationType === 'Preference Percent')?.CalculationValue ?? ''),
+                    })
+                })
+
+                results.push(round_results)
+            });
+
+            return {
+                state: d[0] as State,
+                name: d[1] ?? '',
+                candidates: candidates,
+                results: results,
+            }
+        }),
+    }
+
+    console.log(election_results);
+    return election_results;
+}
