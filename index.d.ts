09775a6f23afd63d24a1f24ab2e028cf22a935ba
oscar-manglaras <257900962+oscar-manglaras@users.noreply.github.com>
2026-04-23 17:59:40 +0930

    remove bezier-js

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,3 @@
 declare module "https://cdn.jsdelivr.net/npm/d3@7/+esm" {
     export * from 'd3';
 }
-
-declare module "https://cdn.jsdelivr.net/npm/bezier-js@6/+esm" {
-    export * from 'bezier-js';
-}
