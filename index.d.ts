e0da77e479b392401d6dadfa2cdc7ed206a643e6
oscar-manglaras <257900962+oscar-manglaras@users.noreply.github.com>
2026-04-23 15:54:03 +0930

    configure eslint and add bezier-js types

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,7 @@
 declare module "https://cdn.jsdelivr.net/npm/d3@7/+esm" {
     export * from 'd3';
 }
+
+declare module "https://cdn.jsdelivr.net/npm/bezier-js@6/+esm" {
+    export * from 'bezier-js';
+}
